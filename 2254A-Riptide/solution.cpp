1f1317525e254e1d03cd1bfb4c9a02974675613f
Deeptam Bhar <deeptambhar@iisc.ac.in>
2026-08-24 06:15:39 +0530

    Accepted: 2254A Riptide (46 ms, 0 KB)

---
+++ 2254A-Riptide/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve(){
+    vector<int> tokens(3);
+    cin >> tokens[0] >> tokens[1] >> tokens[2];
+    int rounds = 0;
+    while (true){
+        sort(tokens.begin(), tokens.end());
+        if ((tokens[0]==tokens[1])||(tokens[1]==tokens[2])){
+            break;
+        }
+        tokens[0]++;
+        tokens[2]--;
+        rounds++;
+    }
+    cout << rounds << "\n";
+}
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
+
