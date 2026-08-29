ec819d2f6a07081d99af54dc328cabeeb4f9fa5d
Deeptam Bhar <deeptambhar@iisc.ac.in>
2026-08-29 20:24:16 +0530

    Accepted: 2258A Odd Eraser (62 ms, 0 KB)

---
+++ 2258A-Odd-Eraser/solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i =0; i<n; ++i){
+        cin >> a[i];
+    }
+    cout << gcd(a[0], a[n-1]) << "\n";
+
+}
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
